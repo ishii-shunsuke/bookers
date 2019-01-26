62410b372af29a55da5380403ac88e8e190ffbfa
石井俊祐 <ishiishunyuu@ishiishunyuunoMacBook-puro.local>
2019-01-26 21:07:20 +0900

    [finish]5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only:[:show,:create,:index,:edit,:update,:destroy]
 
   def show
   	@user = User.find(params[:id])
